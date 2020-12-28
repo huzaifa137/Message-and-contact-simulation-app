8f9a9c33f93f48c67a1aa04111a90d3e92dd5ed6
BUKENYA HUZAIFA <huzaifabukenya227@gmail.com>
2020-12-28 11:22:43 +0530

    adds phonebookApp file

---
+++ phonebookApp.java
@@ -0,0 +1,19 @@
+package github;
+
+import java.util.*;
+
+class in
+{
+	
+	
+	String name,address,email;
+	
+	public in(String name, String address, String email)
+	{
+		
+		this.name=name;
+		this.address=address;
+		this.email=email;	
+	}
+	
+}
