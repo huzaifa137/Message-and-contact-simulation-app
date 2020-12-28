dd494ff8d92dc9ed1e40564495d2de03370ce40a
BUKENYA HUZAIFA <huzaifabukenya227@gmail.com>
2020-12-28 11:25:09 +0530

    adds Hash and insertion logic

--- phonebookApp.java
+++ phonebookApp.java
@@ -17,3 +17,49 @@ class in
 	}
 	
 }
+
+public class phonebookApp
+{
+		
+		static Scanner scan = new Scanner(System.in);
+		
+		static HashMap<Integer,in> d1 = new HashMap<Integer,in>();
+	
+	
+	public static void insert()
+	{
+		
+		System.out.println("Enter phone number");
+		int mk=scan.nextInt();
+		int ln=0;
+		if(d1.containsKey(mk))
+		{
+			System.out.println("The Phone number is already taken");
+			ln=-1;
+		}
+		else
+		{
+			 ln=mk;
+		}
+				
+				while(ln!=-1)
+				{
+				System.out.println("Enter name");
+				String m =scan.next();
+				
+				System.out.println("Enter address");
+				String x=scan.next();
+				
+				System.out.println("Enter email");
+				String y=scan.next();
+				
+				
+				in d2 = new in(m, x, y);
+				
+				d1.put(ln, d2);
+				System.out.println("\n Record is inserted successfully");
+				break;
+				}
+		
+	}
+	
